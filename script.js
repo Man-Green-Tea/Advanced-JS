81dc6a80a9077a79af0cf440d29e3a6d58decc1d
Man-Green-Tea <77284441+Man-Green-Tea@users.noreply.github.com>
2021-10-06 21:32:39 +0300

    Классы

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
-onload = function () {
-  const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-  ];
-
-  const renderGoodsItem = (title, price) =>
-    (`<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`);
-
-  const renderGoodsList = (list) => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+class GoodsItem {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
   }
-
-  renderGoodsList(goods);
-
+  render() {
+    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+  }
+}
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+  fetchGoods() {
+    this.goods = [
+      { title: 'Shirt', price: 150 },
+      { title: 'Socks', price: 50 },
+      { title: 'Jacket', price: 350 },
+      { title: 'Shoes', price: 250 },
+    ];
+  }
+}
+render() {
+  let listHtml = '';
+  this.goods.forEach(good => {
+    const goodItem = new GoodsItem(good.title, good.price);
+    listHtml += goodItem.render();
+  });
+  debugger;
+  document.querySelector('.goods-list').innerHTML = listHtml;
+}
+calcItems() { //так понимаю что дальее это пойдёт в корзину
+  let sum = 0;
+  this.goods.forEach(good => {
+    sum += good.price
+  });
+}
+class CartItem { }
+class CartList { }
+onload = function () {
+  const list = new GoodsList();
+  list.fetchGoods();
+  list.render();
 }
