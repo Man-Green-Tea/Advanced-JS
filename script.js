19c9d9b87bbec36bc101dee5d4f80cc96aab9a59
Man-Green-Tea <77284441+Man-Green-Tea@users.noreply.github.com>
2021-10-11 14:48:12 +0300

    Добавил запрос к API

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+function makeGETRequest(url, callback) {
+  let xhr;
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest;
+  } else if (window.ActiveXObject) {
+    xhr = new ActiveXObject;
+  }
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText);
+    }
+  }
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
 class GoodsItem {
-  constructor(title, price) {
-    this.title = title;
+  constructor(product_name, price) {
+    this.product_name = product_name;
     this.price = price;
   }
   render() {
-    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
   }
 }
 class GoodsList {
   constructor() {
     this.goods = [];
   }
-  fetchGoods() {
-    this.goods = [
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 },
-    ];
+  fetchGoods(callback) {
+    makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+      this.goods = JSON.parse(goods);
+      callback();
+    })
   }
   render() {
     let listHtml = '';
     this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.title, good.price);
+      const goodItem = new GoodsItem(good.product_name, good.price);
       listHtml += goodItem.render();
     });
-    debugger;
     document.querySelector('.goods-list').innerHTML = listHtml;
   }
-  calcItems() { //так понимаю что дальее это пойдёт в корзину
+  calcItems() { //так понимаю что далее это пойдёт в корзину
     let sum = 0;
     this.goods.forEach(good => {
       sum += good.price
@@ -40,6 +56,10 @@ class CartItem { }
 class CartList { }
 onload = function () {
   const list = new GoodsList();
-  list.fetchGoods();
-  list.render();
+  list.fetchGoods(() => {
+    list.render();
+  })
 }
+
+
+
